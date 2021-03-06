e25977986c8d7106eb7f632f0fe3a64eae33800a
lopessgustavo <gustavolp@id.uff.br>
2021-03-06 13:46:50 -0300

    adicao do teste getStatus

--- test/FaturaTest.java
+++ test/FaturaTest.java
@@ -12,28 +12,35 @@ public class FaturaTest {
 
 	private Fatura fatura;
 	
-	public void inicializa(LocalDate data, int valorTotal, String nomeDoCliente ) {
-		fatura = new Fatura(data, valorTotal, nomeDoCliente);
+	public void inicializa(LocalDate data, int valorTotal, String nomeDoCliente, String status) {
+		fatura = new Fatura(data, valorTotal, nomeDoCliente, status);
 	}
 	@DisplayName("Teste GetNome")
 	@Test
 	void testGetNome() {
-		inicializa(LocalDate.now(), 1000, "Gustavo Lopes");
+		inicializa(LocalDate.now(), 1000, "Gustavo Lopes","NAO_PAGA");
 		Assertions.assertEquals("Gustavo Lopes", fatura.getNome());
 	}
 	
 	@Test
 	@DisplayName("Teste getValorTotal")
 	void testGetValorTotal() {
-		inicializa(LocalDate.now(), 1000, "Gustavo Lopes");
+		inicializa(LocalDate.now(), 1000, "Gustavo Lopes", "NAO_PAGA");
 		Assertions.assertEquals(1000, fatura.getValorTotal());
 	}
 	
 	@Test
 	@DisplayName("Teste GetDate")
 	void testGetDate() {
-		inicializa(LocalDate.now(), 1000, "Gustavo Lopes");
+		inicializa(LocalDate.now(), 1000, "Gustavo Lopes", "NAO_PAGA");
 		Assertions.assertEquals(LocalDate.now(), fatura.getDate());
+	}
+	
+	@Test
+	@DisplayName("Teste getStatus")
+	void testGetStatus() {
+		inicializa(LocalDate.now(), 1000, "Gustavo Lopes", "NAO_PAGA");
+		Assertions.assertEquals("NAO_PAGA", fatura.getStatus());
 	}
 
 }
