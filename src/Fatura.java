173dd05be9d3f457eed827dc2a06e6d3a7cc8fc3
lopessgustavo <gustavolp@id.uff.br>
2021-03-06 13:48:26 -0300

    Adicao do campo status na classe Fatura

--- src/Fatura.java
+++ src/Fatura.java
@@ -4,12 +4,13 @@ public class Fatura {
 	private String nome;
 	private LocalDate date;
 	private int valorTotal;
+	private String status;
 	
-	
-	public Fatura(LocalDate data, int valorTotal, String nomeDoCliente ) {
+	public Fatura(LocalDate data, int valorTotal, String nomeDoCliente, String status) {
 		this.nome = nomeDoCliente;
 		this.date = data;
 		this.valorTotal = valorTotal;
+		this.setStatus(status);
 	}
 	
 	
@@ -23,5 +24,19 @@ public class Fatura {
 	}
 	public int getValorTotal() {
 		return valorTotal;
+	}
+
+
+
+
+	public String getStatus() {
+		return status;
+	}
+
+
+
+
+	public void setStatus(String status) {
+		this.status = status;
 	}
 }
